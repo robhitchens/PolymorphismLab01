447395ad8675b50bad3e748beee1f18b96c0d29e
roberthitchens3 <rjhitchens3@gmail.com>
2016-02-03 08:57:54 -0500

    part 2 and 3. forgot to make new branches for parts. woops

---
+++ src/main/java/io/zipcoder/pets/Pet.java
@@ -0,0 +1,16 @@
+package io.zipcoder.pets;
+
+/**
+ * Created by roberthitchens3 on 2/3/16.
+ */
+public class Pet {
+    private String name;
+
+    public String speak(){
+        return null;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){return name;}
+}
--- src/main/java/io/zipcoder/pets/PetThing.java
+++ src/main/java/io/zipcoder/pets/PetThing.java
@@ -9,21 +9,47 @@ import java.util.regex.Pattern;
  * Created by roberthitchens3 on 2/2/16.
  */
 public class PetThing {
-    private ArrayList<String> pets;
+    private ArrayList<Pet> pets;
     public PetThing(){
-        pets = new ArrayList<String>();
+        pets = new ArrayList<Pet>();
         run();
     }
     public void run(){
         int numOfPets = Integer.parseInt(userInput("how many pets do you have? ", "number"));
         for (int i = 0; i<numOfPets; i++){
-            pets.add(userInput("What kind of pet do you have? ","")+userInput("what's your pets' name? ", ""));
+            String type = userInput("What kind of pet do you have? ","");
+            String name = userInput("what's your pets' name? ", "");
+            Pet pet = null;
+            switch(type){
+                case "Dog":
+                    pet = new Dog();
+                    pet.setName(name);
+                    break;
+                case "Cat":
+                    pet = new Cat();
+                    pet.setName(name);
+                    break;
+                case "Hamster":
+                    pet = new Hamster();
+                    pet.setName(name);
+                    break;
+                default:
+                    System.out.println("not a recognized pet.");
+
+            }
+            try {
+                pets.add(pet);
+            }catch(NullPointerException e){
+                    //something
+            }
         }
         printPets();
 
     }
     public void printPets(){
-        System.out.print(pets.toString());
+        for(Pet p : pets){
+            System.out.println(p.getName() + " " + p.speak());
+        }
     }
     public String userInput(String message, String type){
         Scanner scanner = new Scanner(System.in);
